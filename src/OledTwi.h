44780749205c298f33142a920fa7067924b45ed4
nicoud <nicoud@didel.com>
2019-03-28 23:24:17 +0100

    changes 190328
    moved foles to docs, examples, src

--- src/OledTwi.h
+++ src/OledTwi.h
@@ -1,10 +1,9 @@
-// "OledI2C.h" Oled minimal lib using AVR TWI on AVR 328 17611
-// MIT License
-#include <avr/io.h>
-//#include <Arduino.h>
+// "OledTwi.h" minimal transfer lib using AVR TWI on AVR 328 17611
+//#include "OledTwi.h"  // Avant OledPix.h
+//#include <avr/io.h>
 
 #define Adr 0x78  //(0x3C*2)
-void SetupOledI2C() {
+void SetupOledTwi() {
   TWSR = 1;  //0 400kHz 1-160k 2 40k
   TWBR = 0x0C; // bitrate
   TWCR = (1<<TWEN); // autres bits à 0
@@ -21,6 +20,17 @@ void  Write (byte ab) {  // addr8 ou data8
   TWDR = ab;
   TWCR = (1<<TWINT)|(1<<TWEN);
   while (!(TWCR & (1<<TWINT))) {}
-;  status= TWSR & 0xF8;  // Ack bit
+//  status= TWSR & 0xF8;  // Ack bit
 }
+byte TwReadAck () {  
+  TWCR = (1 << TWINT) | (1 << TWEN) |(1<<TWEA);
+  while (!(TWCR & (1 << TWINT))) {}
+  return (TWDR);
+}
+byte TwReadNack () {  
+  TWCR = (1 << TWINT) | (1 << TWEN); 
+  while (!(TWCR & (1 << TWINT))) {}
+  return (TWDR);
+}
+
 
